462eb94624b23d6065deea76390eead798281ec9
Katiurin Dmitry <77469329+xaxaxa1000@users.noreply.github.com>
2022-12-27 15:11:05 +0300

    Добавил аутентификацию
    Добавил файл с аутентификацией, чуть доработал поля

--- php/auth.php
+++ php/auth.php
@@ -1,10 +1,19 @@
 <<?php
-
+$phone_number = filter_var(trim($_POST['phone_number']),FILTER_SANITIZE_STRING);
+$password = filter_var(trim($_POST['password']),FILTER_SANITIZE_STRING);
+$password=md5($password);
+//echo $phone_number;
 $mysql = new mysqli('localhost','root','root','site');
 
-$result =$mysql->query("SELECT phone_number,password FROM `register`");
+$result=$mysql->query("SELECT phone_number,password FROM `register` WHERE
+   `phone_number`='$phone_number' AND `password` = '$password'");
+$user=$result->fetch_assoc();
+if($result->num_rows == 0){
+  echo "Неверный пароль";
+  exit();
+}
+  echo $user['phone_number'],'  ',$user['password'];
+  echo "Логин и пароль верный!";
 
-
-
-
+setcookie('user',$user['phone_number'],time()+60,"/");
  ?>
